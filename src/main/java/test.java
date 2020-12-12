4c6821d74183249210135c15b5cae983678c2f2b
MAX0996 <maximehav@gmail.com>
2020-12-12 14:51:59 +0100

    Max's changes 2

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,6 @@
 
 public class test{
     public static void main(String[] args){
-        System.out.println("yes");
-        System.out.println("Changement fait par Maxime HAVGOUDOUKIAN");
-        System.out.println("hola c'est moi");
-        System.out.println("C'est encore Max qui fait des changements");
+        System.out.println("Il n'y a rien a voir !");
     }
 }
