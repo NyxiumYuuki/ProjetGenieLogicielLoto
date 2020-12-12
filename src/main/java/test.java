8ceb61afea8d890dc39e094b24f1a10921c9e255
MAX0996 <maximehav@gmail.com>
2020-12-12 14:22:34 +0100

    Salut

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@
 public class test{
     public static void main(String[] args){
         System.out.println("yes");
+        System.out.println("Changement fait par Maxime HAVGOUDOUKIAN");
     }
 }
