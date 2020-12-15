b42888dc5d07e9fd948e5bbedc49f9c391787d1b
name <NyxiumYuuki@users.noreply.github.com>
2020-12-15 11:28:32 +0100

    Example files of JavaFX added from openjfx

---
+++ src/main/java/fr/myny/gui/SecondaryController.java
@@ -0,0 +1,13 @@
+package fr.myny.gui;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        Gui.setRoot("primary");
+    }
+}
