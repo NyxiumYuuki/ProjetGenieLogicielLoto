02308ff3069c6bde07c0a3bfffdff082274ce57c
NicolasFabregas <fabregasnicolas@gmail.com>
2020-12-16 20:58:29 +0100

    commit 3 : Connexion (oui)+creationtable(non)

--- src/main/java/fr/myny/database/DataBase.java
+++ src/main/java/fr/myny/database/DataBase.java
@@ -13,14 +13,18 @@ import javax.sql.DataSource;
  */
 
 public class DataBase {
-    Connection connexion;
+    public static String url="jdbc:mariadb://vachot.fr:3306?user=mynynicolas&password=Bw0po64*";
+    Connection conn;
 
-
+    boolean test;
     /**
      * Le constructeur de DataBase
      */
     public DataBase(){
-
+        try{getConnection();}
+        catch(SQLException sqle){
+            System.out.print(sqle);
+        }
     }
 
     /**
@@ -40,117 +44,117 @@ public class DataBase {
     /**
      * La methode de creation de la base de donnees
      */
-    public void createDataBase(String fileName) {
-        /*String url = "jdbc:sqlite:C:/Users/W7/Desktop/" + fileName;
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
-
+    public void createDataBase() {
+        String sql="CREATE TABLE IF NOT EXISTS myny.Test_Table ("+
+            "annee_numero_de_tirage INTEGER,"+
+            "jour_de_tirage CHAR(10),"+
+            "date_de_tirage DATE,"+
+            "date_de_forclusion DATE,"+
+            "boule_1 TINYINT,"+
+            "boule_2 TINYINT,"+
+            "boule_3 TINYINT,"+
+            "boule_4 TINYINT,"+
+            "boule_5 TINYINT,"+
+            "numero_chance TINYINT,"+
+            "combinaison_gagnante_en_ordre_croissantchar(20),"+
+            "nombre_de_gagnant_au_rang1 INTEGER,"+
+            "rapport_du_rang1 INTEGER,"+
+            "nombre_de_gagnant_au_rang2 INTEGER,"+
+            "rapport_du_rang2 INTEGER,"+
+            "nombre_de_gagnant_au_rang3 INTEGER,"+
+            "rapport_du_rang3 INTEGER,"+
+            "nombre_de_gagnant_au_rang4 INTEGER,"+
+            "rapport_du_rang4 INTEGER,"+
+            "nombre_de_gagnant_au_rang5 INTEGER,"+
+            "rapport_du_rang5 INTEGER,"+
+            "nombre_de_gagnant_au_rang6 INTEGER,"+
+            "rapport_du_rang6 INTEGER,"+
+            "numero_jokerplus INTEGER,"+
+            "deviseCHAR(10));";
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            test=stmt.execute(sql);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        }*/
-        //Class.forName("com.mysql.jdbc.Driver");
-        /*try(Connection myCo = DriverManager.getConnection("jdbc:raima:rdm://C:/Users/W7/IdeaProjects/ProjetGenieLogicielLoto/src/"+fileName)){
-            try(Statement mySt = myCo.createStatement()){
-                mySt.execute("Create DataBase Test_DB");
-                mySt.execute("Create Table Test_Table(" +
-                        "annee_numero_de_tirage(integer)," +
-                        "jour_de_tirage(char(10))," +
-                        "date_de_tirage(Date)," +
-                        "date_de_forclusion(Date)," +
-                        "boule_1(tinyint)," +
-                        "boule_2(tinyint)," +
-                        "boule_3(tinyint)," +
-                        "boule_4(tinyint)," +
-                        "boule_5(tinyint)," +
-                        "numero_chance(tinyint)," +
-                        "combinaison_gagnante_en_ordre_croissant(char(20))," +
-                        "nombre_de_gagnant_au_rang1(integer)," +
-                        "rapport_du_rang1(integer)," +
-                        "nombre_de_gagnant_au_rang2(integer)," +
-                        "rapport_du_rang2(integer)," +
-                        "nombre_de_gagnant_au_rang3(integer)," +
-                        "rapport_du_rang3(integer)," +
-                        "nombre_de_gagnant_au_rang4(integer)," +
-                        "rapport_du_rang4(integer)," +
-                        "nombre_de_gagnant_au_rang5(integer)," +
-                        "rapport_du_rang5(integer)," +
-                        "nombre_de_gagnant_au_rang6(integer)," +
-                        "rapport_du_rang6(integer)," +
-                        "numero_jokerplus(integer),"+
-                        "devise(char(3)),"+
-                        ")");
-                myCo.commit();
-                mySt.close();
-            }
-            catch(Exception e){
-                System.out.println(e);
-            }
-            finally {
-
-            }
-            myCo.close();
-         */
-        /*}
-        catch (SQLException sqle) {
-            System.out.println(sqle);
-        }*/
-        /*try {
-            String line;
-            Process p = Runtime.getRuntime().exec("C:/Users/W7/Desktop/database.sql");
-            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while ((line = input.readLine()) != null) {
-                System.out.println(line);
-            }
-            input.close();
         }
-        catch (Exception err) {
-            err.printStackTrace();
+        /*try(Statement mySt = conn.createStatement()){
+            mySt.execute(//"CREATE SCHEMA IF NOT EXISTS Connection;"+
+                    "CREATE TABLE IF NOT EXISTS myny.Test_Table ("+
+                    "annee_numero_de_tirage INTEGER,"+
+                    "jour_de_tirage CHAR(10),"+
+                    "date_de_tirage DATE,"+
+                    "date_de_forclusion DATE,"+
+                    "boule_1 TINYINT,"+
+                    "boule_2 TINYINT,"+
+                    "boule_3 TINYINT,"+
+                    "boule_4 TINYINT,"+
+                    "boule_5 TINYINT,"+
+                    "numero_chance TINYINT,"+
+                    "combinaison_gagnante_en_ordre_croissantchar(20),"+
+                    "nombre_de_gagnant_au_rang1 INTEGER,"+
+                    "rapport_du_rang1 INTEGER,"+
+                    "nombre_de_gagnant_au_rang2 INTEGER,"+
+                    "rapport_du_rang2 INTEGER,"+
+                    "nombre_de_gagnant_au_rang3 INTEGER,"+
+                    "rapport_du_rang3 INTEGER,"+
+                    "nombre_de_gagnant_au_rang4 INTEGER,"+
+                    "rapport_du_rang4 INTEGER,"+
+                    "nombre_de_gagnant_au_rang5 INTEGER,"+
+                    "rapport_du_rang5 INTEGER,"+
+                    "nombre_de_gagnant_au_rang6 INTEGER,"+
+                    "rapport_du_rang6 INTEGER,"+
+                    "numero_jokerplus INTEGER,"+
+                    "deviseCHAR(10));");
+            conn.commit();
+            //mySt.close();
+            //conn.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        finally {
         }*/
-        /*loadDatabase();
-
+        /*
         try {
             PreparedStatement preparedStatement = connexion.prepareStatement("INSERT INTO noms(nom, prenom) VALUES(?, ?);");
             preparedStatement.setString(1, utilisateur.getNom());
             preparedStatement.setString(2, utilisateur.getPrenom());
-
+    
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }*/
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e) {
-        }
-
-        try {
-            connexion = DriverManager.getConnection("jdbc:mysql://localhost:3306");
-        } catch (SQLException e) {
-            System.out.println("oskour " +e.getStackTrace());
-        }
-
-        Statement statement = null;
-        ResultSet resultat = null;
-        try {
-            statement = connexion.createStatement();
-
-            // Exécution de la requête
-            resultat = statement.executeQuery("SELECT nom, prenom FROM noms;");
-
-            // Récupération des données
-            while (resultat.next()) {
-                String nom = resultat.getString("nom");
-                String prenom = resultat.getString("prenom");
-            }
-        }
-        catch (SQLException e) {
-            System.out.println(e);
-        }
+    
+    //    try {
+    //        Class.forName("com.mysql.jdbc.Driver");
+    //    }
+    //    catch (ClassNotFoundException e) {
+    //    }
+    //
+    //    try {
+    //        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306");
+    //    } catch (SQLException e) {
+    //        System.out.println("oskour " +e.getStackTrace());
+    //    }
+    //
+    //    Statement statement = null;
+    //    ResultSet resultat = null;
+    //    try {
+    //        statement = conn.createStatement();
+    //
+    //        // Exécution de la requête
+    //        resultat = statement.executeQuery("SELECT nom, prenom FROM noms;");
+    //
+    //        // Récupération des données
+    //        while (resultat.next()) {
+    //            String nom = resultat.getString("nom");
+    //            String prenom = resultat.getString("prenom");
+    //        }
+    //    }
+    //    catch (SQLException e) {
+    //        System.out.println(e);
+    //    }
     }
 
 
@@ -161,17 +165,33 @@ public class DataBase {
 
     }
     public Connection getConnection() throws SQLException {
-        Connection conn = null;
+        /*Connection conn = null;
         Properties connectionProps = new Properties();
         connectionProps.put("user", "w7");
-        connectionProps.put("password", "asCVhik;9$");
-        if (this.dbms.equals("mysql")) {
+        connectionProps.put("password", "asCVhik;9$");*/
+        /*if (this.dbms.equals("mysql")) {
             conn = DriverManager.getConnection(
                     "jdbc:" + this.dbms + "://" + this.serverName + ":" + this.portNumber + "/", connectionProps);
         } else if (this.dbms.equals("derby")) {
             conn = DriverManager.getConnection("jdbc:" + this.dbms + ":"+this.dbName +";create=true", connectionProps);
+        }*/
+        //System.out.println("Connected to database");
+        conn = null;
+        //Properties connectionProps = new Properties();
+        //connectionProps.put("user", "mynynicolas");
+        //connectionProps.put("password", "Bw0po64*");
+        try {
+            conn = DriverManager.getConnection("jdbc:mariadb://vachot.fr:3306?user=mynynicolas&password=Bw0po64*");
+        /*if (this.dbms.equals("mysql")) {
+            conn = DriverManager.getConnection(
+                    "jdbc:" + this.dbms + "://" + this.serverName + ":" + this.portNumber + "/", connectionProps);
+        } else if (this.dbms.equals("derby")) {
+            conn = DriverManager.getConnection("jdbc:" + this.dbms + ":"+this.dbName +";create=true", connectionProps);
+        }*/
+            System.out.println("Connected to database");
+        }catch(SQLException e) {
+            System.out.println("on a des pb:" + e);
         }
-        System.out.println("Connected to database");
         return conn;
         //
     }
