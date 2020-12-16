034a459e8d94e4691fdd25b6a8401601a60375e5
NicolasFabregas <fabregasnicolas@gmail.com>
2020-12-16 18:50:14 +0100

    commit 2 : plein de tests dans createdatabase et getConnection et ca marche pas

---
+++ src/test/java/fr/myny/database/DataBaseTest.java
@@ -0,0 +1,28 @@
+package fr.myny.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataBaseTest {
+
+    @Test
+    void fillDataBase() {
+    }
+
+    @Test
+    void importDataBase() {
+    }
+
+    @Test
+    void createDataBase() {
+    }
+
+    @Test
+    void updateDataBase() {
+    }
+
+    @Test
+    void getConnection() {
+    }
+}
