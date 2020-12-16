7cca3b75222b3416e27d4a68358ecc11bde736cc
yann <yann_tho@outlook.fr>
2020-12-16 19:23:55 +0100

    modif DataCsv.java et repertoir test

--- src/main/java/fr/myny/data/ImportData.java
+++ src/main/java/fr/myny/data/ImportData.java
@@ -22,4 +22,8 @@ public class ImportData {
      */
     public void getUrl(){
     }
+
+    public void unzip() {
+
+    }
 }
---
+++ src/test/java/fr/myny/data/DataCsvTest.java
@@ -0,0 +1,12 @@
+package fr.myny.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DataCsvTest {
+
+    @Test
+    void getCsv() {
+
+    }
+}
